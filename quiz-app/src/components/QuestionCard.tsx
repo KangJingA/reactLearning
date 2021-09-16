5bbb632b03d56da73dd9f9a48c6820f8bbde0142
Nicholas Ang <66071629+KangJingA@users.noreply.github.com>
2021-09-16 19:51:50 +0800

    removed any types

--- quiz-app/src/components/QuestionCard.tsx
+++ quiz-app/src/components/QuestionCard.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+import { AnswerObject } from "../App";
 // must declare the type of the props now
 type Props = {
   question: string;
   answers: string[]; // array of strings
-  callback: any;
-  userAnswer: any;
+  callback: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNumber: number;
   totalQuestions: number;
 };
@@ -25,10 +27,10 @@ const QuestionCard: React.FC<Props> = ({
     <p dangerouslySetInnerHTML={{ __html: question }}></p>
     {answers.map((answer) => (
       <div key={answer}>
-        <button disabled={userAnswer} value={answer} onClick={callback}>
+        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
           {/* <span dangerouslySetInnerHTML={{ __html: answer }}></span>
            */}
-           {answer}
+          {answer}
         </button>
       </div>
     ))}
